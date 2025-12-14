bdbb5ffb8ecd28cadfee38aba99fa9ff19a8efac
Omer Hamelech <omersher9175@gmail.com>
2025-12-14 13:02:55 +0200

    update

---
+++ SherioConsoleApp/Program (2).cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Model;
+using ApiInterface;
+
+namespace ApiInterface
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            ApiService apiService = new ApiService("http://localhost:5064");
+
+            Console.ReadLine();
+        }
+    }
+}
